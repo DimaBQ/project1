4f27d97881024cf89a8604f340749ae3eed968d5
DimaBQ <butdm123@gmail.com>
2018-06-22 10:24:56 +0300

    This is programm write and run english words!

---
+++ study_word.rb
@@ -0,0 +1,18 @@
+puts "Hello. You will be learning a lot of english words and you can add your words in the list
+By the way, I hope you will a good student.
+Lets go study English!!!
+Для добавления своих слов нажмите 1
+Для изучения слов нажмите 2
+Для того чтобы вывести полный список слов нажмите 3"
+
+choice_user = gets.chomp
+
+if choice_user == 1
+  
+elsif choice_user == 2
+
+elsif choice_user == 3
+
+else
+
+end
